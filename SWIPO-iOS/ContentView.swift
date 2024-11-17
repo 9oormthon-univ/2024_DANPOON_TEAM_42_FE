dac3852ac1ea4db9bd24438cec69ff0dec32d15c
wodnd <wodnd0418@naver.com>
2024-11-18 00:09:42 +0900

    Initial Commit

---
+++ SWIPO-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWIPO-iOS
+//
+//  Created by wodnd on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
