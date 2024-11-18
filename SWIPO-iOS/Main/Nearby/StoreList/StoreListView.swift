8bff8e6a68cd8a75b7f95044843d9c1e4a18149f
Bibi-urssu <bibi.urssu@gmail.com>
2024-11-18 17:28:42 +0900

    [FEAT] StoreModel 및 StoreListView UI 구현

---
+++ SWIPO-iOS/Main/Nearby/StoreList/StoreListView.swift
@@ -0,0 +1,234 @@
+//
+//  StoreListView.swift
+//  SWIPO-iOS
+//
+//  Created by 박지윤 on 11/18/24.
+//
+
+import SwiftUI
+
+struct StoreListView: View {
+
+    let stores: [Store] = sampleStores
+
+    @State var searchText: String = ""
+
+    @State var optionModal: Bool = false
+    @State var option: String = ""
+
+    @State var categoryModal: Bool = false
+    @State var category: String = ""
+
+    var body: some View {
+        ZStack {
+            VStack {
+                NavigationBar(title: "내 주변 가맹점", showBackButton: true)
+                    .padding(.horizontal, 17)
+                
+                HStack {
+                    Image("search")
+                        .resizable()
+                        .frame(width: 24 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                        .foregroundColor(Color.greyLightActive)
+                    CustomTextField(
+                        placeholder: "지역 및 가맹점을 검색해 보세요",
+                        text: $searchText,
+                        placeholderColor: UIColor.gray,
+                        textColor: UIColor.black, iconColor: .greyLightActive
+                    )
+                }
+                .padding(.vertical, 3)
+                .padding(.horizontal, 12)
+                .frame(height: 40 * Constants.ControlHeight)
+                .background(Color.white)
+                .cornerRadius(14)
+                .padding(.horizontal, 17)
+                .shadow(radius: 2)
+                .padding(.top, 8)
+                
+                HStack {
+                    Button(action: {
+                        categoryModal.toggle()
+                    }) {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(width: 87 * Constants.ControlWidth, height: 34 * Constants.ControlHeight)
+                            .foregroundColor(.greyDark)
+                            .overlay {
+                                HStack {
+                                    Text("전체")
+                                        .foregroundColor(Color.white)
+                                        .font(.Body2)
+                                    Image("down")
+                                        .resizable()
+                                        .frame(width: 24 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                    }
+                    
+                    Button(action: {
+                        optionModal.toggle()
+                    }) {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(width: 116 * Constants.ControlWidth, height: 34 * Constants.ControlHeight)
+                            .foregroundColor(.greyDark)
+                            .overlay {
+                                HStack {
+                                    Text("가까운 순")
+                                        .foregroundColor(Color.white)
+                                        .font(.Body2)
+                                    Image("down")
+                                        .resizable()
+                                        .frame(width: 24 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                                        .foregroundColor(Color.white)
+                                }
+                            }
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, 8)
+                
+                ScrollView {
+                    ForEach(stores) { store in
+                        StoreRowView(store: store)
+                            .padding(.horizontal)
+                            .padding(.bottom, 33)
+                    }
+                }
+                .padding(.top, 20)
+                .padding(.bottom, 40)
+            }.background(Color.black)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                    }) {
+                        Circle()
+                            .frame(width: 42 * Constants.ControlWidth, height: 42 * Constants.ControlHeight)
+                            .foregroundColor(.greyLightHover)
+                            .overlay {
+                                Image("up")
+                                    .resizable()
+                                    .frame(width: 24 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                                    .foregroundColor(.black)
+                            }
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 10)
+                
+                Button(action: {
+                }, label: {
+                    RoundedRectangle(cornerRadius: 16)
+                        .frame(width: 360 * Constants.ControlWidth, height: 54 * Constants.ControlHeight)
+                        .foregroundColor(.mainNormal)
+                        .overlay {
+                            Text("결제하기")
+                                .foregroundColor(Color.white)
+                                .font(.Subhead3)
+                        }
+                })
+            }
+        }
+        .navigationBarBackButtonHidden()
+    }
+
+    struct ClearBackgroundView: UIViewRepresentable {
+        func makeUIView(context: Context) -> some UIView {
+            let view = UIView()
+            DispatchQueue.main.async {
+                view.superview?.superview?.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+            }
+            return view
+        }
+        func updateUIView(_ uiView: UIViewType, context: Context) {
+        }
+    }
+    
+    struct ClearBackgroundViewModifier: ViewModifier {
+        
+        func body(content: Content) -> some View {
+            content
+                .background(ClearBackgroundView())
+        }
+    }
+}
+
+struct StoreRowView: View {
+    let store: Store
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ZStack(alignment: .topTrailing) {
+                Image(store.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 160)
+                    .clipped()
+                Image(store.isFavorite ? .heartFilled : .heart)
+                    .resizable()
+                    .frame(width: 27 * Constants.ControlWidth, height: 27 * Constants.ControlHeight)
+                    .padding(10)
+                    .foregroundColor(.white)
+            }
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(store.name)
+                    .font(.Headline)
+                    .foregroundColor(.white)
+                Text(store.address)
+                    .font(.Body2)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 6)
+                        .frame(width: 80 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                        .foregroundColor(.greyNormal)
+                        .overlay {
+                            Text(store.point)
+                                .foregroundColor(Color.white)
+                                .font(.Subhead2)
+                        }
+
+                    if store.isHot {
+                        RoundedRectangle(cornerRadius: 6)
+                            .frame(width: 61 * Constants.ControlWidth, height: 24 * Constants.ControlHeight)
+                            .foregroundColor(.greyNormal)
+                            .overlay {
+                                Text("HOT🔥")
+                                    .foregroundColor(Color.white)
+                                    .font(.Subhead2)
+                            }
+                    }
+
+                    Spacer()
+                    
+                    HStack(spacing: 2) {
+                        Image("favorite")
+                            .resizable()
+                            .frame(width: 16 * Constants.ControlWidth, height: 16 * Constants.ControlHeight)
+                        Text("\(store.rating, specifier: "%.1f")")
+                            .font(.Subhead3)
+                        Text("(\(store.reviewCount))")
+                            .font(.Subhead2)
+                            .foregroundColor(.greyLightHover)
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .padding(.top, 12)
+            .padding(.horizontal, 10)
+        }
+    }
+}
+
+#Preview {
+    StoreListView()
+}
+
---
+++ SWIPO-iOS/Main/Nearby/StoreList/StoreModel.swift
@@ -0,0 +1,26 @@
+//
+//  StoreModel.swift
+//  SWIPO-iOS
+//
+//  Created by 박지윤 on 11/18/24.
+//
+
+import Foundation
+
+struct Store: Identifiable {
+    let id = UUID()
+    let name: String
+    let address: String
+    let imageName: String
+    let point: String
+    let rating: Double
+    let reviewCount: Int
+    let isFavorite: Bool
+    let isHot: Bool
+}
+
+let sampleStores = [
+    Store(name: "맛찬들왕소금구이 울산 무거점", address: "울산 남구 신복로22번길 21", imageName: "sample1", point: "Point 5%", rating: 5.0, reviewCount: 35, isFavorite: true, isHot: false),
+    Store(name: "의도카페", address: "울산 남구 대학로52번길 5-17 1층", imageName: "sample2", point: "Point 15%", rating: 4.5, reviewCount: 549, isFavorite: false, isHot: true),
+    Store(name: "울리정", address: "울산 울주군 청량읍 울밀로영해1길 170-6", imageName: "sample3", point: "Point 10%", rating: 4.8, reviewCount: 89, isFavorite: true, isHot: false)
+]
